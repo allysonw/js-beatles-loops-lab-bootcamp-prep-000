90d510c4bc77ac4c95360bd20c2ef771f587cfdb
Allyson Wesman <allysonwesman@gmail.com>
2017-12-24 18:18:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    newArray[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts){
+  for (let i = 0; i < facts.length
+}
